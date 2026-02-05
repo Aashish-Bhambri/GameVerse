602e7f3f2043d10555e0248bd89a69aa3f34d591
Codewiizard <135721204+Codewiizard@users.noreply.github.com>
2026-02-05 13:32:33 +0530

    Refactor game query handling: consolidate genre and platform state into a single GameQuery object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,6 @@
 
+import type { GameQuery } from "@/App";
 import useData from "./useData";
-import type { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -16,9 +16,9 @@ export interface Game {
 }
 
  
-const useGames = (selectedGenre:Genre | null,selectedPlatform:Platform |null) => useData<Game>('/games',{
-    params:{genres:selectedGenre?.id,
-    platforms:selectedPlatform?.id}
-},[selectedGenre?.id,selectedPlatform?.id]);
+const useGames = (gameQuery:GameQuery) => useData<Game>('/games',{
+    params:{genres:gameQuery.genre?.id,
+    platforms:gameQuery.platform?.id}
+},[gameQuery]);
 
 export default useGames;
