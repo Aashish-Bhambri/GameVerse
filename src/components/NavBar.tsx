4c275c47ad09c3cb7f2233fef5b89e921b7a80ba
Codewiizard <135721204+Codewiizard@users.noreply.github.com>
2026-02-09 22:28:44 +0530

    Refactor NavBar to include SearchInput component and update image placeholder handling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import react from "../assets/react.svg";
 import ChangecolorMode from "./ChangecolorMode";
+import { SearchInput } from "./SearchInput";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={react}></Image>
+      <SearchInput />
       <ChangecolorMode />
     </HStack>
   );
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,12 @@
+import { Input } from "@chakra-ui/react";
+import React from "react";
+
+export const SearchInput = () => {
+  return (
+    <Input
+      placeholder="Search games.."
+      borderRadius={20}
+      variant="outline"
+    ></Input>
+  );
+};
